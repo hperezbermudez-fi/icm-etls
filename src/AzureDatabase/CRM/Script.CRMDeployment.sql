c83cda964deb578908618a6a52c053635ba80dac
Hardys Perez Bermudez <60011073+hardys-perez@users.noreply.github.com>
2022-06-13 07:28:48 -0700

    Scripts to create tables and stored procedures for CRM files

---
+++ src/AzureDatabase/CRM/Script.CRMDeployment.sql
@@ -0,0 +1,64 @@
+﻿:r .\tables\crm.sql
+:r .\tables\crm_archive.sql
+
+:r .\sps\usp_etl_crm.sql
+:r .\sps\usp_post_crm.sql
+:r .\sps\usp_post_export_crm.sql
+:r .\sps\usp_pre_crm.sql
+:r .\sps\usp_pre_export_crm.sql
+
+INSERT INTO [comm].[VaricentApiConfiguration]
+    (
+    [RecordId]
+    ,[FileType]
+    ,[FolderName]
+    ,[FolderId]
+    )
+    VALUES
+        (
+            2
+            ,'CRM'
+            ,'CRM'
+            ,23
+        )
+GO
+
+INSERT INTO [comm].[ConfigOnAzure]
+           ([RecordId]
+           ,[OnPremSchemaName]
+           ,[OnPremViewName]
+           ,[AzureSchemaName]
+           ,[AzureTableName]
+           ,[PreOnPremSP]
+           ,[PostOnPremSP]
+           ,[PreAzureSP]
+           ,[PostAzureSP]
+           ,[ExportSP]
+           ,[PreExportSP]
+           ,[PostExportSP]
+           ,[ExportedFileNamePrefix]
+           ,[ExportedFileNamePattern]
+           ,[ProcessStatus]
+           ,[LastRunId]
+           ,[StartTime]
+           ,[LastProcessedTime])
+     VALUES
+           (2
+           ,'comm'
+           ,'COMM_CRM_Relationship_Details_All'
+           ,'comm'
+           ,'crm'
+           ,'[comm].usp_pre_crm'
+           ,'[comm].usp_post_crm'
+           ,'[comm].usp_pre_crm'
+           ,'[comm].usp_post_crm'
+           ,'[comm].usp_etl_crm'
+           ,'[comm].usp_pre_export_crm'
+           ,'[comm].usp_post_export_crm'
+           ,'crm'
+           ,'{FileNamePrefix}_{ICMRunId}_yyyyMMdd_hhmmss.psv'
+           ,NULL
+           ,NULL
+           ,NULL
+           ,NULL)
+GO
---
+++ src/AzureDatabase/CRM/tables/crm.sql
@@ -0,0 +1,37 @@
+﻿--Comm.COMM_CRM_Relationship_Details_All
+
+CREATE TABLE [comm].[CRM](
+	[OnyxID] [varchar](18) NOT NULL,
+	[fi_SalutationLetter] [varchar](220) NULL,
+	[FirstName] [varchar](100) NULL,
+	[MiddleName] [varchar](50) NULL,
+	[LastName] [varchar](100) NULL,
+	[Suffix] [varchar](60) NULL,
+	[Client_Name] [varchar](255) NULL,
+	[User_Type] [varchar](4000) NULL,
+	[User_SubType] [varchar](4000) NULL,
+	[Country_CD] [varchar](4) NULL,
+	[SourceID] [varchar](4000) NULL,
+	[OID_LOA_Dt] [datetime] NULL,
+	[OID_FTD] [datetime] NULL,
+	[vchOSP] [varchar](400) NULL,
+	[vchIC] [varchar](400) NULL,
+	[Relationship_Value] [money] NULL,
+	[chResidentCountry] [varchar](4) NULL,
+	[vchServiceCountry] [varchar](160) NULL,
+	[E_Code_Dt] [varchar](4000) NULL,
+	[ChgToFormerClient_Dt] [datetime] NULL,
+	[IRIS_EnvelopeSalutation] [varchar](100) NULL,
+	[IRIS_LetterSalutation] [varchar](220) NULL,
+	[AOR_Address1] [varchar](4000) NULL,
+	[AOR_Address2] [varchar](4000) NULL,
+	[AOR_Address3] [varchar](4000) NULL,
+	[AOR_City] [varchar](4000) NULL,
+	[AOR_Region] [varchar](4000) NULL,
+	[AOR_PostCode] [varchar](50) NULL,
+	[AOR_Country] [varchar](4000) NULL,
+	[SecureEmail] [varchar](250) NULL,
+	[NonSecureEmail] [varchar](250) NULL
+	,CONSTRAINT [PK_CRM] PRIMARY KEY ([OnyxID])
+) ON [PRIMARY]
+GO
