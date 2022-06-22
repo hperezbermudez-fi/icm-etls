9cc2df0e782d9c43e0244eddda4fd0ddf7579ffa
Hardys Perez Bermudez <60011073+hardys-perez@users.noreply.github.com>
2022-06-22 06:30:57 -0700

    Remove unnecessary fields from CRM/CRM_Archive tables
    Update SP for CRM Transformation

--- src/AzureDatabase/CRM/tables/crm.sql
+++ src/AzureDatabase/CRM/tables/crm.sql
@@ -10,28 +10,7 @@ CREATE TABLE [comm].[CRM](
 	[Client_Name] [varchar](255) NULL,
 	[User_Type] [varchar](4000) NULL,
 	[User_SubType] [varchar](4000) NULL,
-	[Country_CD] [varchar](4) NULL,
-	[SourceID] [varchar](4000) NULL,
-	[OID_LOA_Dt] [datetime] NULL,
-	[OID_FTD] [datetime] NULL,
 	[vchOSP] [varchar](400) NULL,
-	[vchIC] [varchar](400) NULL,
-	[Relationship_Value] [money] NULL,
-	[chResidentCountry] [varchar](4) NULL,
-	[vchServiceCountry] [varchar](160) NULL,
-	[E_Code_Dt] [varchar](4000) NULL,
-	[ChgToFormerClient_Dt] [datetime] NULL,
-	[IRIS_EnvelopeSalutation] [varchar](100) NULL,
-	[IRIS_LetterSalutation] [varchar](220) NULL,
-	[AOR_Address1] [varchar](4000) NULL,
-	[AOR_Address2] [varchar](4000) NULL,
-	[AOR_Address3] [varchar](4000) NULL,
-	[AOR_City] [varchar](4000) NULL,
-	[AOR_Region] [varchar](4000) NULL,
-	[AOR_PostCode] [varchar](50) NULL,
-	[AOR_Country] [varchar](4000) NULL,
-	[SecureEmail] [varchar](250) NULL,
-	[NonSecureEmail] [varchar](250) NULL
-	
+	[E_Code_Dt] [varchar](4000) NULL
 ) ON [PRIMARY]
 GO
