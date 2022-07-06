911610fce0fc8454cd9598d13e972c59ca0743e4
hperezbermudez-fi <h.perezbermudez@fi.com>
2022-07-06 11:34:30 -0700

    Add ConfirmedSplit File

---
+++ src/AzureDatabase/ConfirmedSplit/tables/confirmed_split_archive.sql
@@ -0,0 +1,18 @@
+﻿
+CREATE TABLE [comm].[ConfirmedSplit_Archive](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ICMRunId] [int] NULL,
+	[RecordId] [int] NULL,
+	[ClientID] [int] NULL,
+	[EffectiveDate] [varchar](50) NULL,
+	[EffectiveEndDate] [varchar](50) NULL,
+	[EmployeeID] [int] NOT NULL,
+	[Percentage] [varchar](25) NOT NULL,
+	[CreatedByName] [varchar](100) NULL,
+	[CreatedOnDateTime] [datetime] NULL,
+ CONSTRAINT [PK_ConfirmedSplit_Archive_Archive] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
