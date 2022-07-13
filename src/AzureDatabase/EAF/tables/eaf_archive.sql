de1c72218943587b3c2585cd59dca2b5a916e7fb
hperezbermudez-fi <h.perezbermudez@fi.com>
2022-07-13 14:47:31 -0700

    Add ETL for EAF

---
+++ src/AzureDatabase/EAF/tables/eaf_archive.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [comm].[EAF_Archive](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ICMRunId] [int] NULL,
+	[RecordId] [int] NULL,
+	[RecordIDKey] [varchar](150) not null,
+	[CID]	   [varchar](32) null,
+	[MonthEnd] [varchar](50) null,
+	[RunDate]  [varchar] (50) null,
+	[RunType]  [varchar](3) null,
+	[dBillingRateAmount] [varchar](50) null,
+	[CreatedByName] [varchar](100) NULL,
+	[CreatedOnDateTime] [datetime] NULL,
+ CONSTRAINT [PK_EAF_Archive] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
